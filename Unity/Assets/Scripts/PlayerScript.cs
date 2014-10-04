e04626c4bdd2ba897204d77d8e690fb05c920499
Adam Leung <centribo@hotmail.com>
2014-10-04 11:02:59 -0400

    Added Player Two, changed inputs.

--- Unity/Assets/Scripts/PlayerScript.cs
+++ Unity/Assets/Scripts/PlayerScript.cs
@@ -7,6 +7,7 @@ public class PlayerScript : MonoBehaviour
 		public int speed = 5;
 		public int jumpForce = 70;
 		bool isOnGround = false;
+		public int playerNumber = 0;
 	
 		// Use this for initialization
 		void Start ()
@@ -17,15 +18,32 @@ public class PlayerScript : MonoBehaviour
 		// Update is called once per frame
 		void Update ()
 		{
-				bool left = Input.GetButton ("leftKey");
-				bool right = Input.GetButton ("rightKey");
-				bool up = Input.GetButton ("upKey");
-				if (right) {
-						this.transform.Translate (Vector2.right * speed * Time.deltaTime);
-				} else if (left) {
-						this.transform.Translate (-Vector2.right * speed * Time.deltaTime);
+				bool jump = Input.GetButton ("JumpKey");
+
+				switch (playerNumber) {
+				case 1:
+						bool playerOneLeft = Input.GetButton ("PlayerOneLeftKey");
+						bool playerOneRight = Input.GetButton ("PlayerOneRightKey");
+			
+						if (playerOneRight) {
+								this.transform.Translate (Vector2.right * speed * Time.deltaTime);
+						} else if (playerOneLeft) {
+								this.transform.Translate (-Vector2.right * speed * Time.deltaTime);
+						}
+						break;
+				case 2:
+						bool playerTwoLeft = Input.GetButton ("PlayerTwoLeftKey");
+						bool playerTwoRight = Input.GetButton ("PlayerTwoRightKey");
+			
+						if (playerTwoRight) {
+								this.transform.Translate (Vector2.right * speed * Time.deltaTime);
+						} else if (playerTwoLeft) {
+								this.transform.Translate (-Vector2.right * speed * Time.deltaTime);
+						}
+						break;
 				}
-				if (up && isOnGround) {
+				
+				if (jump && isOnGround) {
 						this.rigidbody2D.AddForce (new Vector2 (0, jumpForce));
 						isOnGround = false;
 				}
@@ -33,7 +51,7 @@ public class PlayerScript : MonoBehaviour
 	
 		void OnCollisionEnter2D (Collision2D c)
 		{
-				if (c.transform.tag == "Floor" || c.transform.tag == "Switch") {
+				if (c.transform.tag == "Platform" || c.transform.tag == "Switch") {
 						isOnGround = true;
 				}
 		}
